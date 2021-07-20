9134078345cc59d1d958a9d7419e0093b68d1f47
h1-the-swan <jason.portenoy@gmail.com>
2021-07-19 17:49:19 -0700

    initial setup and requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='spotify_personal',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Access and modify personal spotify account',
+    author='Jason Portenoy'
+)
